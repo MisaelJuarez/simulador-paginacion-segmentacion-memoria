de309c64df96a2de3ce55d04d3aaf37473079a2e
Misael Juarez <138533025+MisaelJuarez@users.noreply.github.com>
2023-09-22 18:40:00 -0600

    Memoria llena

--- paginacion/main.js
+++ paginacion/main.js
@@ -6,35 +6,46 @@ let memoria = ['','','','','','','',''];
 $btnIniciarProceso.addEventListener('click', () => {
     const $tipoProceso = document.getElementById('lista-procesos').value;
     const $tamanioProceso = document.getElementById('tamanio-proceso').value;
+    let espacioEnMemoria = 0;
 
-    let p = document.createElement("p");
-    p.textContent = $tipoProceso;
-    p.classList.add('p');
-    $procesosActivos.appendChild(p);
+    memoria.forEach((e) => {
+        if (e === '') espacioEnMemoria++;
+    });
 
-    let $paguinas = document.querySelectorAll('.pagina');
-    let $arrayPaguinas = Array.from($paguinas);
+    if (espacioEnMemoria >= $tamanioProceso) {
+        let p = document.createElement("p");
+        p.textContent = $tipoProceso;
+        p.classList.add('p');
+        $procesosActivos.appendChild(p);
     
-    for (let i = 0; i < parseInt($tamanioProceso); i++) {
-        for (let j = 0; j < $arrayPaguinas.length; j++) {
-            if (memoria[j] === '') {
-                $arrayPaguinas[j].textContent = $tipoProceso;
-                memoria[j] = $tipoProceso;
-                if ($tipoProceso == 'A') {
-                    $arrayPaguinas[j].classList.add('color-memoria-a');
-                } else if($tipoProceso == 'B'){
-                    $arrayPaguinas[j].classList.add('color-memoria-b');
-                } else if($tipoProceso == 'C'){
-                    $arrayPaguinas[j].classList.add('color-memoria-c');
-                } else if($tipoProceso == 'D'){
-                    $arrayPaguinas[j].classList.add('color-memoria-d');
-                }else if($tipoProceso == 'E'){
-                    $arrayPaguinas[j].classList.add('color-memoria-e');
+        let $paguinas = document.querySelectorAll('.pagina');
+        let $arrayPaguinas = Array.from($paguinas);
+        
+        for (let i = 0; i < parseInt($tamanioProceso); i++) {
+            for (let j = 0; j < $arrayPaguinas.length; j++) {
+                if (memoria[j] === '') {
+                    $arrayPaguinas[j].textContent = $tipoProceso;
+                    memoria[j] = $tipoProceso;
+                    if ($tipoProceso == 'A') {
+                        $arrayPaguinas[j].classList.add('color-memoria-a');
+                    } else if($tipoProceso == 'B'){
+                        $arrayPaguinas[j].classList.add('color-memoria-b');
+                    } else if($tipoProceso == 'C'){
+                        $arrayPaguinas[j].classList.add('color-memoria-c');
+                    } else if($tipoProceso == 'D'){
+                        $arrayPaguinas[j].classList.add('color-memoria-d');
+                    }else if($tipoProceso == 'E'){
+                        $arrayPaguinas[j].classList.add('color-memoria-e');
+                    }
+                    break;
                 }
-                break;
             }
         }
+    } else {
+        alert('No hay suficiente espacio');
     }
+
     console.log(memoria);
+    console.log(espacioEnMemoria);
 });
 
